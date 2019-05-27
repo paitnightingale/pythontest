2a18cb8abab95091bbfb23310f60c6bbade6054c
张三 <zhangsan@163.com>
2019-05-27 21:08:25 +0800

    num02

--- cpts_649_bpo/001.py
+++ cpts_649_bpo/001.py
@@ -10,10 +10,16 @@ app.config['SECRET_KEY'] = '123456'
 
 
 
+
 # 当报404时，网页就会走
 @app.errorhandler(404)
 def inter_error(e):
     return render_template('commin/index.html')
+
+def nume02()
+	num02 = 20
+
+
 # 注册蓝图
 app.register_blueprint(admin_blu, url_prefix='/admin')
 app.register_blueprint(index_blu, url_prefix='/index')
